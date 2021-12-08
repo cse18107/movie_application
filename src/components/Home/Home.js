61e139d4aab63a736f06d93508e03292fe0ab979
cse18107 <cse18107@cemk.ac.in>
2021-12-08 12:05:11 +0530

    working on store and api

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,13 +2,20 @@ import React, { useState, useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../common/apis/movieApi";
 import { APIKey } from "../../common/apis/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
+
 
 const Home = () => {
   const [data, setData] = useState([]);
   const movieText = "iron man";
+  const dispatch = useDispatch()
   useEffect(() => {
     fetchMovies();
+    dispatch(addMovies(data));
   }, []);
+
+  
 
   async function fetchMovies() {
     try {
@@ -18,7 +25,7 @@ const Home = () => {
 
       const res = await response.json();
 
-      console.log("The response from api: " + res.Search.length);
+      console.log("The response from api: " + res.Search);
       setData(res.Search);
     } catch (error) {
       console.log(error);
@@ -28,30 +35,11 @@ const Home = () => {
   return (
     <div>
       <div className="banner-img"></div>
-      <ul>
-        <li style={{ "text-decoration": "none", "list-style": "none", }}>
-          {data.map((ele)=>{
-            return <div className="card" style={{ "width": "18rem","margin":"20px" }}>
-            <img src={ele.Poster} className="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">{ele.Title}</h5>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="btn" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-          })}
-        </li>
-      </ul>
-      {/* <MovieListing /> */}
+      
+      <MovieListing />
     </div>
   );
 };
 
 export default Home;
 
-//https://www.omdbapi.com/?s=thor&page=1&apikey=65b31166
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./features/store";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
